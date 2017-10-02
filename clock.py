d84048acc250c71a894ad1d72f5c2785033f6dc7
stash4 <high.mackey@gmail.com>
2017-10-02 20:01:47 +0900

    [Fix] clock.py修正

--- clock.py
+++ clock.py
@@ -4,13 +4,13 @@ import os
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('interval', minutes=5, end_date='2017-10-02 10:59:59')
+@scheduler.scheduled_job('interval', minutes=5, end_date='2017-10-02 11:10:00')
 def timed_job():
     print('Run notifier')
     os.system('python main.py')
 
 
-@scheduler.scheduled_job('cron', year=2017, month=10, day=2, hour=11, minute=5)
+@scheduler.scheduled_job('cron', year=2017, month=10, day=2, hour=11, minute=10)
 def scheduled_job():
     print('result')
     os.system('python result.py')
