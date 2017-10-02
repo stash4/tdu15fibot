11d2f37f490dd775f15cb04dc9e5cc8f809069d5
stash4 <high.mackey@gmail.com>
2017-10-02 20:02:38 +0900

    [Fix] clock.py修正2

--- clock.py
+++ clock.py
@@ -10,7 +10,7 @@ def timed_job():
     os.system('python main.py')
 
 
-@scheduler.scheduled_job('cron', year=2017, month=10, day=2, hour=11, minute=10)
+@scheduler.scheduled_job('cron', year=2017, month=10, day=2, hour=11, minute=15)
 def scheduled_job():
     print('result')
     os.system('python result.py')
