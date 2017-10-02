f23376e78dedf98e7667303fe1a30dbdc8bb89e8
stash4 <high.mackey@gmail.com>
2017-10-02 14:07:38 +0900

    [Mod] 登録数取得部分のモジュール化

--- main.py
+++ main.py
@@ -6,23 +6,9 @@ import requests
 import time
 import urllib
 from tweet import tweet
+from scraping import get_table
 
-# 配属希望登録サイトへのログイン情報
-login_info = {
-    'id': os.environ['TDU_ID'],
-    'code': os.environ['TDU_PASS'],
-    'func': 'authByRadius'
-}
-url_login = 'https://www.mlab.im.dendai.ac.jp/bthesis2018/StudentDeploy.jsp'
-
-# ログインしてresponse取得
-session = requests.session()
-res = session.post(url_login, data=login_info, timeout=30)
-res.raise_for_status()
-
-# soupで現在の希望登録数のtableを取得
-soup = BeautifulSoup(res.text, 'html.parser')
-table = soup.find('table', {'class': 'remain_table'}).findAll('td')
+table = get_table()
 
 # db接続
 urllib.parse.uses_netloc.append('postgres')
